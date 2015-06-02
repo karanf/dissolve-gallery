222dab3f85d8ad82477d32949b4b782d2029a274
karanf <karanf@gmail.com>
2015-06-02 16:38:52 +1200

    More functionality added
    Added navigation buttons & functionality
    changed border style
    jQuery for dissolving images with timer

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
 $(document).ready(function(){
 	var $thumbs = $('.thumbs img'),
 		$images = $('.images img'),
-		currentIndex = 0;
+		$nextBtn = $('.next-btn'),
+		$prevBtn = $('.prev-btn'),
+		lastIndex = $images.length-1,
+		currentIndex = 0,
+		delay = 4000,
+		timer;
 
+	function changeImage(newIndex){
+		resetTimer();
+		$images.eq(currentIndex).velocity('fadeOut', {duration: 600});
+		$thumbs.eq(currentIndex).removeClass('selected');
+		currentIndex = newIndex;
+		$images.eq(currentIndex).velocity('fadeIn', {duration: 200});
+		$thumbs.eq(currentIndex).addClass('selected');
+	}
+
+	function nextImage(){
+		var newIndex = (currentIndex < lastIndex)? currentIndex+1 : 0;
+		changeImage(newIndex);
+	}
+
+	function startTimer() {
+		timer = setInterval(nextImage, delay)
+	}
+
+	function resetTimer() {
+		clearInterval(timer);
+		startTimer();
+	}
 
 	//Initialisation
 	$images.hide().eq(currentIndex).show();
 	$thumbs.eq(currentIndex).addClass('selected');
 
+	timer = setInterval(nextImage, delay);
+
 	$thumbs.click(function(){
-		$images.eq(currentIndex).velocity('fadeOut', {duration: 600});
-		$thumbs.eq(currentIndex).removeClass('selected');
-		currentIndex = $thumbs.index(this);
-		$images.eq(currentIndex).velocity('fadeIn', {duration: 200});
-		$thumbs.eq(currentIndex).addClass('selected');
+		var newIndex = $thumbs.index(this);
+		changeImage(newIndex);
+	});
+
+	$nextBtn.click(nextImage);
+
+	$prevBtn.click(function(){
+		var newIndex =(currentIndex > 0)? currentIndex-1 : lastIndex;
+		changeImage(newIndex);
 	});
 });
-
-// Click thumbnail
-
-// - Change the displayed image to the corresponding index value
-
-// - Transition dissolve
-
-// - Selected state for the thumbnail
