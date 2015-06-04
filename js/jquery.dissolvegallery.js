b7759ac53fe925d42e52f0f5c355bbef7b9f5dfe
karanf <karanf@gmail.com>
2015-06-04 17:04:44 +1200

    Made into plugins
    Created plugins of the js files
    minified them
    added them to plugins.js

---
+++ js/jquery.dissolvegallery.js
@@ -0,0 +1,59 @@
+(function($){
+	$.fn.dissolveGallery = function(){
+		return this.each(function(){
+
+		var $this = $(this),
+			$thumbs = $this.find('.thumbs img'),
+			$images = $this.find('.images img'),
+			$nextBtn = $this.find('.next-btn'),
+			$prevBtn = $this.find('.prev-btn'),
+			lastIndex = $images.length-1,
+			currentIndex = 0,
+			delay = 4000,
+			timer;
+
+		function changeImage(newIndex){
+			resetTimer();
+			$images.eq(currentIndex).velocity('fadeOut', {duration: 600});
+			$thumbs.eq(currentIndex).removeClass('selected');
+			currentIndex = newIndex;
+			$images.eq(currentIndex).velocity('fadeIn', {duration: 200});
+			$thumbs.eq(currentIndex).addClass('selected');
+		}
+
+		function nextImage(){
+			var newIndex = (currentIndex < lastIndex)? currentIndex+1 : 0;
+			changeImage(newIndex);
+		}
+
+		function startTimer() {
+			timer = setInterval(nextImage, delay)
+		}
+
+		function resetTimer() {
+			clearInterval(timer);
+			startTimer();
+		}
+
+		//Initialisation
+		$images.hide().eq(currentIndex).show();
+		$thumbs.eq(currentIndex).addClass('selected');
+
+		timer = setInterval(nextImage, delay);
+
+		$thumbs.click(function(){
+			var newIndex = $thumbs.index(this);
+			changeImage(newIndex);
+		});
+
+		$nextBtn.click(nextImage);
+
+		$prevBtn.click(function(){
+			var newIndex =(currentIndex > 0)? currentIndex-1 : lastIndex;
+			changeImage(newIndex);
+		});
+
+		})
+	
+	}
+})(jQuery)
